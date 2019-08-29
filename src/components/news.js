cf529171bd61ed5b8c6a8c3f5116a4b6fb498a46
cedmail <cmailleux@jahia.com>
2019-08-29 11:42:22 -0400

    Fix issue with non existing images

--- src/components/news.js
+++ src/components/news.js
@@ -1,40 +1,48 @@
-import React from "react"
+import React from 'react';
 import Layout from '../components/layout';
-import { graphql } from 'gatsby'
-import {Image,Transformation} from 'cloudinary-react';
-import dateformat from 'dateformat'
+import {graphql} from 'gatsby';
+import {Image, Transformation} from 'cloudinary-react';
+import dateformat from 'dateformat';
 
-export default ({ data }) => {
-  const news = data.jahia.newsEntryById
-  return (
-    <Layout>
-      <h1>{news.title}</h1>
-      <div>Posted at: {dateformat(new Date(news.metadata.created), "fullDate")}</div>
-      <Image cloudName={news.image.cloudinary.cloudspace} publicId={news.image.cloudinary.path} dpr="auto" responsive>
+function cloudinaryNewsImage(image) {
+    if(image !== null){
+        return <Image cloudName={image.cloudinary.cloudspace} publicId={image.cloudinary.path} dpr="auto"
+                      responsive>
             <Transformation height="300" width="300" crop="fill" fetchFormat="auto"/>
         </Image>
-      <div dangerouslySetInnerHTML={{__html:news.body}}/>          
-    </Layout>
-  )
+    }
+    return null;
+}
+
+export default ({data}) => {
+    const news = data.jahia.newsEntryById;
+    return (
+        <Layout>
+            <h1>{news.title}</h1>
+            <div>Posted at: {dateformat(new Date(news.metadata.created), 'fullDate')}</div>
+            {cloudinaryNewsImage(news.image)}
+            <div dangerouslySetInnerHTML={{__html: news.body}}/>
+        </Layout>
+    );
 }
 
 export const query = graphql`
-  query($newsId: String!) {
-    jahia {
-        newsEntryById(id: $newsId) {
-          body
-          title
-          metadata {
-            created
-            lastModified
-          }
-          image {
-            cloudinary {
-              cloudspace
-              path
+    query($newsId: String!) {
+        jahia {
+            newsEntryById(id: $newsId) {
+                body
+                title
+                metadata {
+                    created
+                    lastModified
+                }
+                image {
+                    cloudinary {
+                        cloudspace
+                        path
+                    }
+                }
             }
-          }
         }
-      }
-  }
-`
+    }
+`;
--- src/pages/news.js
+++ src/pages/news.js
@@ -1,42 +1,47 @@
-import React from "react"
+import React from 'react';
 import Layout from '../components/layout';
-import { graphql } from 'gatsby'
-import dateformat from 'dateformat'
-import {Image,Transformation} from 'cloudinary-react';
+import {graphql} from 'gatsby';
+import dateformat from 'dateformat';
+import {Image, Transformation} from 'cloudinary-react';
 
-
-
-export default ({ data }) => (
-  <Layout>
-    <h1>News</h1>
-    {data.jahia.allNews.filter(function(news) {return news.image !== null} ).map((news, i) => (
-      <a key={i} href={news.title.toLowerCase().replace(/\s/g,"")}>
-        <h2>
-          {news.title} - {dateformat(new Date(news.metadata.created), "fullDate")}
-        </h2>
-        <Image cloudName={news.image.cloudinary.cloudspace} publicId={news.image.cloudinary.path} dpr="auto" responsive>
+function cloudinaryNewsImage(image) {
+    if (image !== null) {
+        return <Image cloudName={image.cloudinary.cloudspace} publicId={image.cloudinary.path} dpr="auto" responsive>
             <Transformation height="120" width="300" crop="fill" fetchFormat="auto"/>
-        </Image>
-      </a>
-    ))}
-  </Layout>
+        </Image>;
+    }
+    return null;
+}
+
+export default ({data}) => (
+    <Layout>
+        <h1>News</h1>
+        {data.jahia.allNews.map((news, i) => (
+            <a key={i} href={news.title.toLowerCase().replace(/\s/g, '')}>
+                <h2>
+                    {news.title} - {dateformat(new Date(news.metadata.created), 'fullDate')}
+                </h2>
+                {cloudinaryNewsImage(news.image)}
+            </a>
+        ))}
+    </Layout>
 )
 
 export const query = graphql`
-  query {
-    jahia {
-        allNews {          
-          title
-          metadata {
-              created
-          }
-          image {
-            cloudinary {
-              cloudspace
-              path
+    query {
+        jahia {
+            allNews {
+                title
+                metadata {
+                    created
+                }
+                image {
+                    cloudinary {
+                        cloudspace
+                        path
+                    }
+                }
             }
-          }
         }
-      }
-  }
-`
+    }
+`;
